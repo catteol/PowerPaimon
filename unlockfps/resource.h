8f9b13ac41efaef733ab71747ef6932efee72acf
catteol <catteol@catteol.dev>
2021-10-14 01:57:26 +0900

    Add About menu

--- unlockfps/resource.h
+++ unlockfps/resource.h
@@ -12,13 +12,15 @@
 #define ID_60                           40013
 #define ID_QUIT                         40014
 #define ID_90                           40016
+#define ID_40017                        40017
+#define ID_ABOUT                        40018
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40017
+#define _APS_NEXT_COMMAND_VALUE         40019
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
