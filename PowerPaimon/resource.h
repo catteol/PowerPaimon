e87fa3fdfa6bb9a6db1b7a65243dace897a4b76b
catteol <catteol@catteol.dev>
2021-10-15 17:02:51 +0900

    Add borderless mode and Japanese UI

---
+++ PowerPaimon/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// PowerPaimon.rc で使用
+//
+#define IDI_ICON1                       101
+#define IDR_MENU1                       102
+#define IDS_STRING103                   103
+#define IDS_MENU_BORDERLESS             103
+#define IDS_TOAST_CONFNOTFOUND_TITLE    10001
+#define IDS_TOAST_CONFNOTFOUND_MSG      10002
+#define IDS_TOAST_MISSGAME_TITLE        10003
+#define IDS_TOAST_MISSGAME_MSG          10004
+#define IDS_TOAST_ENABLE_BORDERLESS_TITLE 10005
+#define IDS_TOAST_DISABLE_BORDERLESS_TITLE 10006
+#define IDS_TOAST_NEED_REBOOT_MSG       10007
+#define ID_240                          40008
+#define ID_200                          40009
+#define ID_170                          40010
+#define ID_144                          40011
+#define ID_75                           40012
+#define ID_60                           40013
+#define ID_QUIT                         40014
+#define ID_90                           40016
+#define ID_ABOUT                        40018
+#define ID_LANGUAGE_ENGLISH             40021
+#define ID_BORDERLESS                   40023
+#define ID_LANGUAGE_JAPANESE            40024
+#define ID_MENU_LANGUAGE                40025
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40031
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
