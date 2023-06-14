9320a0e9fc93d8da3eaa9788781d95483bfc9d56
catteol <catteol@catteol.dev>
2023-06-14 14:30:15 +0900

    Update: v2.0.0 merge & brushup

---
+++ PowerPaimon/AboutForm.h
@@ -0,0 +1,132 @@
+﻿#pragma once
+
+namespace unlockfpsclr {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for AboutForm
+	/// </summary>
+	public ref class AboutForm : public Form
+	{
+	public:
+		AboutForm(void)
+		{
+			InitializeComponent();
+			
+			this->Load += gcnew EventHandler(this, &AboutForm::OnLoad);
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~AboutForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ labelTitle;
+
+
+
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(AboutForm::typeid));
+			this->labelTitle = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// labelTitle
+			// 
+			this->labelTitle->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(134)));
+			this->labelTitle->Location = System::Drawing::Point(12, 119);
+			this->labelTitle->Name = L"labelTitle";
+			this->labelTitle->Padding = System::Windows::Forms::Padding(0, 5, 0, 0);
+			this->labelTitle->Size = System::Drawing::Size(271, 33);
+			this->labelTitle->TabIndex = 0;
+			this->labelTitle->Text = L"PowerPaimon v0.0.0.0";
+			this->labelTitle->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(271, 104);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 4;
+			this->pictureBox1->TabStop = false;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->BackColor = System::Drawing::SystemColors::Control;
+			this->richTextBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->richTextBox1->Location = System::Drawing::Point(12, 180);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->ReadOnly = true;
+			this->richTextBox1->Size = System::Drawing::Size(271, 164);
+			this->richTextBox1->TabIndex = 5;
+			this->richTextBox1->Text = resources->GetString(L"richTextBox1.Text");
+			this->richTextBox1->LinkClicked += gcnew System::Windows::Forms::LinkClickedEventHandler(this, &AboutForm::richTextBox1_LinkClicked);
+			this->richTextBox1->Enter += gcnew System::EventHandler(this, &AboutForm::richTextBox1_Enter);
+			// 
+			// label1
+			// 
+			this->label1->Location = System::Drawing::Point(12, 152);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(271, 12);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Copyright © 2023 rexent_gx";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// AboutForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(296, 356);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->labelTitle);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
+			this->Name = L"AboutForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterParent;
+			this->Text = L"About";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: Void OnLoad(Object^ sender, EventArgs^ e);
+	private: System::Void richTextBox1_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkClickedEventArgs^ e);
+	private: System::Void richTextBox1_Enter(System::Object^ sender, System::EventArgs^ e);
+};
+}
--- PowerPaimon/resource.h
+++ PowerPaimon/resource.h
@@ -1,38 +1,74 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ で生成されたインクルード ファイル。
-// PowerPaimon.rc で使用
+// Microsoft Visual C++ generated include file.
+// Used by PowerPaimon.rc
 //
-#define IDI_ICON1                       101
-#define IDR_MENU1                       102
-#define IDS_STRING103                   103
-#define IDS_MENU_BORDERLESS             103
-#define IDS_TOAST_CONFNOTFOUND_TITLE    10001
-#define IDS_TOAST_CONFNOTFOUND_MSG      10002
-#define IDS_TOAST_MISSGAME_TITLE        10003
-#define IDS_TOAST_MISSGAME_MSG          10004
-#define IDS_TOAST_ENABLE_BORDERLESS_TITLE 10005
-#define IDS_TOAST_DISABLE_BORDERLESS_TITLE 10006
-#define IDS_TOAST_NEED_REBOOT_MSG       10007
-#define ID_240                          40008
-#define ID_200                          40009
-#define ID_170                          40010
-#define ID_144                          40011
-#define ID_75                           40012
-#define ID_60                           40013
-#define ID_QUIT                         40014
-#define ID_90                           40016
-#define ID_ABOUT                        40018
-#define ID_LANGUAGE_ENGLISH             40021
-#define ID_BORDERLESS                   40023
-#define ID_LANGUAGE_JAPANESE            40024
-#define ID_MENU_LANGUAGE                40025
+#define IDS_ERROR                       100
+#define IDS_BROWSE                      101
+#define IDS_CONFIRM                     102
+#define IDR_DLL1                        103
+#define IDS_CONFIGURE                   103
+#define IDR_DLL2                        104
+#define IDS_EXIT                        104
+#define IDR_DLL3                        105
+#define IDS_INFO                        105
+#define IDR_DLL4                        106
+#define IDR_DLL5                        107
+#define IDR_DLL6                        108
+#define IDR_DLL7                        109
+#define IDR_DLL8                        110
+#define IDI_ICON1                       121
+#define IDS_SETUP_TITLE                 1000
+#define IDS_SETUP_DIALOG_BADEXE         1001
+#define IDS_SETUP_DIALOG_WRONGPLACE     1002
+#define IDS_SETUP_LBRESULT_FOUND        1003
+#define IDS_SETUP_LBSELECT_FOUND        1004
+#define IDS_SETUP_LBRESULT_NOTFOUND     1005
+#define IDS_SETUP_LBSELECT_NOTFOUND     1006
+#define IDS_SETUP_SELECT_HINT           1007
+#define IDS_MAIN_TITLE                  2000
+#define IDS_MAIN_MENU_FILE              2001
+#define IDS_MAIN_MENU_SETUP             2002
+#define IDS_MAIN_MENU_ABOUT             2003
+#define IDS_MAIN_TT_AUTOLAUNCH          2004
+#define IDS_MAIN_MENU_LANG              2005
+#define IDS_MAIN_MENU_LANG_JA           2006
+#define IDS_MAIN_MENU_LANG_EN           2007
+#define IDS_MAIN_MENU_LANG_DEFAULT      2008
+#define IDS_MAIN_TAB_GENERAL            2009
+#define IDS_MAIN_TAB_OPT                2010
+#define IDS_MAIN_TAB_DLL                2011
+#define IDS_MAIN_CHK_MINIMIZE           2012
+#define IDS_MAIN_CHK_AUTOEXIT           2013
+#define IDS_MAIN_CHK_PWRSAVE            2014
+#define IDS_MAIN_DRD_PROCPRIORITY       2015
+#define IDS_MAIN_CHK_VSYNC              2016
+#define IDS_MAIN_CHK_POPUP              2017
+#define IDS_MAIN_CHK_FULLSCREEN         2018
+#define IDS_MAIN_CHK_CUSTOMRES          2019
+#define IDS_MAIN_LB_WINMODE             2020
+#define IDS_MAIN_LB_MONITOR             2021
+#define IDS_MAIN_LB_DLLCAP              2022
+#define IDS_MAIN_BTN_ADD                2023
+#define IDS_MAIN_BTN_REMOVE             2024
+#define IDS_MAIN_CHK_AUTOLAUNCH         2025
+#define IDS_MAIN_BTN_Launch             2026
+#define IDS_MAIN_BTN_LAUNCH             2026
+#define IDS_MAIN_BTN_Launch             2026
+#define IDS_MAIN_BTN_LAUNCH             2026
+#define IDS_MAIN_MENU_EDIT              2027
+#define IDS_MAIN_TT_PWRSAVE             2028
+#define IDS_MAIN_TT_MINIMIZE            2029
+#define IDS_MAIN_TT_AUTOEXIT            2030
+#define IDS_ABOUT_MESSAGE               3000
+#define IDS_NOTIFY_MINIMIZE_MSG         3001
+#define IDS_DIAG_MSG_RUNNING            3002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40031
+#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
