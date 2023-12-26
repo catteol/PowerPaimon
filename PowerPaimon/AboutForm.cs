961bc7bcd2a288e26022ba35558faed743e2596d
catteol <catteol@catteol.dev>
2023-12-27 02:26:22 +0900

    Merge netcore update

---
+++ PowerPaimon/AboutForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerPaimon
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkLabelSource_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenLink("https://github.com/rexent-gx/PowerPaimon");
+        }
+
+        private void LinkLabelIssues_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenLink("https://github.com/rexent-gx/PowerPaimon/issues");
+        }
+
+        private void OpenLink(string url)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+        }
+
+    }
+}
